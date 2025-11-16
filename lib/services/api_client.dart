aaca3948daeb1ed2e68b9e108011abe6eccd158c
Munira870 <muniraalsaleem2@gmail.com>
2025-11-16 23:06:55 +0300

    Update api_client.dart

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -334,6 +334,34 @@ static Future<void> uploadLicenseUpdateFile({
     throw Exception('فشل رفع ملف الرخصة: HTTP ${response.statusCode}: $respBody');
   }
 }
+//delete lawyer function
+  static Future<void> deleteLawyer({
+    required int lawyerId,
+    required String base,
+  }) async {
+    final res = await http.post(
+      Uri.parse('$base/delete_lawyer.php'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'LawyerID': lawyerId}),
+    );
 
+    if (res.statusCode != 200) {
+      if (res.statusCode == 409) {
+        // decline delete (lawyer have appoinments)
+        throw Exception('LAWYER_HAS_APPOINTMENTS');
+      }
+      throw Exception('HTTP ${res.statusCode}: ${res.body}');
+    }
+
+    final body = json.decode(res.body);
+    if (body is! Map || body['ok'] != true) {
+      final code = (body is Map) ? (body['code']?.toString() ?? '') : '';
+      final msg = (body is Map) ? (body['message']?.toString() ?? 'Failed to delete lawyer') : '';
+      if (code.toUpperCase().contains('APPOINT')) {
+        throw Exception('LAWYER_HAS_APPOINTMENTS');
+      }
+      throw Exception(msg);
+    }
+  }
 }
 
