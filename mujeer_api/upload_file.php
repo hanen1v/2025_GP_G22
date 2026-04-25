f0ee486f36739e104720857bd0c092ef0b7a1648
itzLama <162923190+itzLama@users.noreply.github.com>
2026-04-25 23:11:37 +0300

    Add files via upload

---
+++ mujeer_api/upload_file.php
@@ -0,0 +1,31 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$targetDir = "uploads/";
+
+if (!file_exists($targetDir)) {
+    mkdir($targetDir, 0777, true);
+}
+
+if (isset($_FILES["file"])) {
+
+    $fileName = time() . "_" . basename($_FILES["file"]["name"]);
+    $targetFile = $targetDir . $fileName;
+
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+
+       echo json_encode([
+    "success" => true,
+    "file_url" => "http://10.0.2.2:8888/mujeer_api/" . $targetFile,
+    "file_name" => basename($_FILES["file"]["name"])
+]);
+
+    } else {
+        echo json_encode(["success" => false]);
+    }
+
+} else {
+    echo json_encode(["success" => false]);
+}
+?>
